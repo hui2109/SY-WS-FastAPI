128a3f28cd18884dd9f745898193cab85ff17870
Zhang_Xuhui <995631664@qq.com>
2025-04-18 22:17:36 +0800

    移除未使用的router变量

--- WSBackends/utils.py
+++ WSBackends/utils.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 from fastapi.templating import Jinja2Templates
-from fastapi import APIRouter
 
 # 设置模板目录
 current_dir = Path(__file__).resolve().parent
@@ -10,7 +9,6 @@ templates_path = parent_dir / "WSFrontends" / "templates"
 Templates = Jinja2Templates(directory=templates_path)
 
 # 设置静态文件目录
-router = APIRouter()
 current_dir = Path(__file__).resolve().parent
 parent_dir = current_dir.parent
 Statics = parent_dir / "WSFrontends"
